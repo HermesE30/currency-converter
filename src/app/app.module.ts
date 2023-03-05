b0e7bea2667c62889de6ea48908932668eb96d8a
Hermes Emmanuel <hermesemmanueldev@gmail.com>
2023-03-05 15:10:42 -0300

    fix layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 // router
 import { AppRoutingModule } from './app-routing.module';
@@ -23,7 +24,8 @@ import { ButtonComponent } from './components/button/button.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
